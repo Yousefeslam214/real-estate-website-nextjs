5a654143df9d68e27661c70c908f98fe67893a39
Yousef eslam <yousefeslam214@gmail.com>
2025-08-02 14:06:06 +0300

    Updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  experimental: {
+    staleTime: {
+      Dynamic: 30 * 1000, // 30 seconds in milliseconds
+    },
+  },
 };
 
 module.exports = nextConfig;
