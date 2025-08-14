58ab29a23dfbbb4312df0c59a6e4da957e5de990
Yousef eslam <yousefeslam214@gmail.com>
2025-08-14 19:33:44 +0300

    fix deploy errors

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import Services from "./components/Services";
 import About from "./components/About";
 import News from "./components/NewsComponents/News";
 import Contact from "./components/Contact";
-import { Suspense } from "react";
-import { ButtonLoading } from "./components/ButtonLoading";
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
